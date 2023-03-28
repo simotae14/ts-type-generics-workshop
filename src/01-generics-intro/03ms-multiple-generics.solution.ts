cffff5f024807c9f357ac264df39f37b8f01e4c7
Simone Taeggi <startae14@gmail.com>
2023-03-28 17:03:34 +0200

    exercise use Multiple Generics in the args of a function

--- src/01-generics-intro/03ms-multiple-generics.solution.ts
+++ src/01-generics-intro/03ms-multiple-generics.solution.ts
@@ -1,7 +1,7 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const returnBothOfWhatIPassIn = (a: unknown, b: unknown) => {
+const returnBothOfWhatIPassIn = <T, U>(a: T, b: U) => {
   return {
     a,
     b,
