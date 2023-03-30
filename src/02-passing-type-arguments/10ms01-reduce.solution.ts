acb1c6da2cacff41bc76a0b8f776ff32ef3d121c
Simone Taeggi <startae14@gmail.com>
2023-03-30 17:51:36 +0200

    exercise Type the reduce method

--- src/02-passing-type-arguments/10ms01-reduce.solution.ts
+++ src/02-passing-type-arguments/10ms01-reduce.solution.ts
@@ -10,7 +10,7 @@ const array = [
   },
 ];
 
-const obj = array.reduce((accum, item) => {
+const obj = array.reduce<Record<string, {name: string}>>((accum, item) => {
   accum[item.name] = item;
   return accum;
 }, {});
---
+++ src/02-passing-type-arguments/10ms03-reduce.solution.ts
@@ -0,0 +1,30 @@
+import { expect, it } from "vitest";
+import { Equal, Expect } from "../helpers/type-utils";
+
+const array = [
+  {
+    name: "John",
+  },
+  {
+    name: "Steve",
+  },
+];
+
+// assign the type to the accumulator directly
+const obj = array.reduce((accum: Record<string, {name: string}>, item) => {
+  accum[item.name] = item;
+  return accum;
+}, {});
+
+it("Should resolve to an object where name is the key", () => {
+  expect(obj).toEqual({
+    John: {
+      name: "John",
+    },
+    Steve: {
+      name: "Steve",
+    },
+  });
+
+  type tests = [Expect<Equal<typeof obj, Record<string, { name: string }>>>];
+});
